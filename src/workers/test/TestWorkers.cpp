00ef0e503eb3bbab6a9b770a28830e2e79651597
Danny Browning <bdbrowning2@gmail.com>
2013-03-01 07:31:31 -0700

    fix compile error in workers, add install capability to package build

--- src/workers/test/TestWorkers.cpp
+++ src/workers/test/TestWorkers.cpp
@@ -1,5 +1,4 @@
 #include "workers/Manager.h"
-#include "workers/TaskQueueManager.h"
 #include "workers/Worker.h"
 #include "workers/Task.h"
 
@@ -196,94 +195,6 @@ TEST(WORKERS_TEST, MANAGER_TEST)
 
         //less workers than tasks, make sure they can go back and grab tasks
         Manager manager(2);
-
-        bool tasksCompleted = true;
-
-        //run some tasks
-        manager.run(tasks[0]);
-        manager.run(tasks[1]);
-        manager.run(tasks[2]);
-        manager.run(tasks[3]);
-
-        {
-            tasksCompleted &= tasks[0]->wasCompletedSuccessfully();
-        }
-
-        //run some more
-        manager.run(tasks[4]);
-        manager.run(tasks[5]);
-        manager.run(tasks[6]);
-        manager.run(tasks[7]);
-        manager.run(tasks[8]);
-        {
-            tasksCompleted &= tasks[3]->wasCompletedSuccessfully();
-        }
-
-        manager.waitForTasksToComplete();
-
-        int tasksToCheck[] = {1,2,4,5,6,7,8};
-
-        for(size_t i = 0; i < 7; ++i)
-        {
-            tasksCompleted &= tasks[tasksToCheck[i]]->wasCompletedSuccessfully();
-        }
-
-        ASSERT_TRUE(tasksCompleted);
-
-        manager.shutdown();
-
-        manager.run(tasks[9]);
-
-        {
-            ASSERT_FALSE(tasks[9]->wasCompletedSuccessfully());
-        }
-
-        //make sure cleanup shuts down correctly
-    }
-}
-
-TEST(WORKERS_TEST, TASK_QUEUE_MANAGER_TEST)
-{
-    {
-        //setup a bunch of tasks
-        std::vector< std::shared_ptr<Task> > tasks;
-        for(size_t i = 0; i < 5; ++i) 
-        {
-            Task* task = new TestTask();
-            tasks.push_back(std::shared_ptr<Task>(task));
-        }
-
-        //less workers than tasks, make sure they can go back and grab tasks
-        TaskQueueManager manager(2);
-
-        for(std::vector< std::shared_ptr<Task> >::const_iterator task = tasks.begin(); task != tasks.end(); ++task)
-        {
-            manager.run((*task));
-        }
-
-        bool tasksCompleted = true;
-
-        for(std::vector< std::shared_ptr<Task> >::const_iterator task = tasks.begin(); task != tasks.end(); ++task)
-        {
-            tasksCompleted &= (*task)->wasCompletedSuccessfully();
-        }
-
-        ASSERT_TRUE(tasksCompleted);
-
-        //make sure cleanup shuts down correctly
-    }
-
-    {
-        //setup a bunch of tasks
-        std::vector< std::shared_ptr<Task> > tasks;
-        for(size_t i = 0; i < 10; ++i) 
-        {
-            Task* task = new TestTask();
-            tasks.push_back(std::shared_ptr<Task>(task));
-        }
-
-        //less workers than tasks, make sure they can go back and grab tasks
-        TaskQueueManager manager(2);
 
         bool tasksCompleted = true;
 
